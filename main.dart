f3bb7029bb9a9b419bb34acc39ff9f7009039fd5
musman-js <usman2037966@gmail.com>
2024-02-24 19:30:25 +0500

    Dart Practice

---
+++ main.dart
@@ -0,0 +1,33 @@
+
+main(){
+  num percent = 56;
+  if(percent >=80 && percent<=100){
+    print("A+");
+  }
+  else if(percent>=70 && percent<80){
+    print("A");
+  }
+  else if(percent>=60 && percent<70){
+    print("B");
+  }
+  else if(percent>=50 && percent<60){
+    print("C");
+  }
+  else if(percent>=0 && percent<40){
+    print("Fail");
+  }
+  else{
+    print("Invalid");
+  }
+
+
+  int age = 17;
+  if(age >=18){
+    print("License ban Jayega");
+  }
+  else{
+    print('License nh banega');
+  }
+
+   
+}
