2839120fd37afdbf51f0c4dc1995d1fbdbde8209
musman-js <usman2037966@gmail.com>
2024-02-26 15:10:06 +0500

    Dart Practicing

--- main.dart
+++ main.dart
@@ -1,40 +1,50 @@
 
 main(){
-  num percent = 56;
-  if(percent >=80 && percent<=100){
-    print("A+");
-  }
-  else if(percent>=70 && percent<80){
-    print("A");
-  }
-  else if(percent>=60 && percent<70){
-    print("B");
-  }
-  else if(percent>=50 && percent<60){
-    print("C");
-  }
-  else if(percent>=0 && percent<40){
-    print("Fail");
-  }
-  else{
-    print("Invalid");
-  }
+  // num percent = 56;
+  // if(percent >=80 && percent<=100){
+  //   print("A+");
+  // }
+  // else if(percent>=70 && percent<80){
+  //   print("A");
+  // }
+  // else if(percent>=60 && percent<70){
+  //   print("B");
+  // }
+  // else if(percent>=50 && percent<60){
+  //   print("C");
+  // }
+  // else if(percent>=0 && percent<40){
+  //   print("Fail");
+  // }
+  // else{
+  //   print("Invalid");
+  // }
 
 
-  int age = 17;
-  if(age >=18){
-    print("License ban Jayega");
-  }
-  else{
-    print('License nh banega');
-  }
+  // int age = 17;
+  // if(age >=18){
+  //   print("License ban Jayega");
+  // }
+  // else{
+  //   print('License nh banega');
+  // }
 
 
-  double num4 = 234.3432;
-  int number = 344;
+  // double num4 = 234.3432;
+  // int number = 344;
 
-  print(number);
-  print(num4);
+  // print(number);
+  // print(num4);
+
+
+  // for(var i = 0; i<=10; i++){
+  //   print(i);
+  // }
+
+
+
+
+
 
 
    
